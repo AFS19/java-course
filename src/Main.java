8d2fc2e129a269afd6b1bbfc0d7b0af4e6107fac
AFS19 <iireco20@gmail.com>
2025-02-16 17:54:49 +0100

    mafs: first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String [] args) {
+        // comment
+
+        /* multi-line comment */
+
+        System.out.println("Hello");
+        System.out.println();
+    }
+}
