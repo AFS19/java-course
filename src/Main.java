eb8cbe8c9104787c520c58e996087821608c49d6
AFS19 <iireco20@gmail.com>
2025-02-16 18:38:45 +0100

    mafs: variables (primitive[stack] & reference[heap])

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,45 @@ public class Main {
 
         /* multi-line comment */
 
-        System.out.println("Hello");
-        System.out.println();
+        // System.out.println("Hello");
+
+        // 2 steps to creating a variable
+        // 1. declare the variable
+        // 2. assign a value to the variable
+
+        int age1; // declaration
+        age1 = 31; // assignment
+        int age = 30; // declaration and assignment's in one line
+
+        // System.out.println(age);
+        // System.out.println(age1);
+
+        int year = 2025;
+        // System.out.println(year);
+        // System.out.println("The year is " + year);
+
+        double price = 4.99;
+        double gpa = 3.5;
+        // System.out.println("My gpa is " + gpa);
+
+        char letter = 'A';
+        // System.out.println(letter);
+
+        boolean isMale = true;
+        // System.out.println(isMale);
+
+        boolean isFemale = false;
+        // System.out.println(isFemale);
+
+        String name = "Mohamed";
+        // System.out.println(name);
+
+        String address = "123 Main St";
+        // System.out.println(address);
+
+        // System.out.println("My name is " + name + " and I live at " + address);
+
+        // System.out.println("My name is " + name + " and I live at " + address + " and I am " + age + " years old");
+
     }
 }
